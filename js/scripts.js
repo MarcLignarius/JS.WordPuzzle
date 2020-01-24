b64188c38b94297f383d2ab0d934c66ac63e819f
Marc Davies <marcdaviesriot@gmail.com>
2020-01-24 14:22:34 -0800

    add function to retrieve user input and save it in a variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(function() {
-
+    $("#userForm").submit(function(event) {
+        event.preventDefault();
+        //retrieve sentence entered by user, save it as userInput
+        let userInput = $("#userInput")
+          .val()
+        // validates that something was submitted in the form
+        if (userInput == "") {
+            alert("This field is required.");
+            return false;
+        }
+        console.log("userInput", userInput)
+    });
 });
