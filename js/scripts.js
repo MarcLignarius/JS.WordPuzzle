fcceeb05d6716208961069e5284be01b6c63a00e
Marc Davies <marcdaviesriot@gmail.com>
2020-01-24 14:37:50 -0800

    cleanup code

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@ $(function() {
     $("#userForm").submit(function(event) {
         event.preventDefault();
         //retrieve sentence entered by user, save it as userInput
-        let userInput = $("#userInput")
-          .val()
+        let userInput = $("#userInput").val();
         // validates that something was submitted in the form
         if (userInput == "") {
-            alert("This field is required.");
+            $("#output").text("This field is required.");
             return false;
         }
         console.log("userInput", userInput)
