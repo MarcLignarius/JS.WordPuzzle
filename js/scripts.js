eff8048b835069b2d3f67de0046a0071e4cb1d04
Marc Davies <marcdaviesriot@gmail.com>
2020-01-25 14:31:55 -0800

    display wordPuzzle in output div

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ $(function() {
     event.preventDefault();
     //retrieve sentence entered by user, save it as userInput
     let userInput = $("#userInput").val();
+    let form = document.getElementById("userForm");
+    form.reset();
     // validates that something was submitted in the form
     if (userInput == "") {
       $("#output").text("This field is required.");
       return false;
     }
-    console.log("userInput", userInput);
     let splitInput = replaceVowels(userInput);
-    console.log("splitInput", splitInput);
     let wordPuzzle = displayPuzzle(splitInput);
-    console.log("wordPuzzle", wordPuzzle);
+    $("#output").text(wordPuzzle);
   });
 
   function replaceVowels(userInput) {
